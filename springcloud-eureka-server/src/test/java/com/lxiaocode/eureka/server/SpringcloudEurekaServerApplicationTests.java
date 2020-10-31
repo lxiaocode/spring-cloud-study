d0b6555838703e663dedf594143cbcdab4ea2dc0
lxiao <lxiaofeng101@gmail.com>
2020-10-31 15:03:35 +0800

    Eureka 服务注册中心

---
+++ springcloud-eureka-server/src/test/java/com/lxiaocode/eureka/server/SpringcloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxiaocode.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
