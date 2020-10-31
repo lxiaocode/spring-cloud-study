8aa7704c9a436c66b61c03ed83b68926878def2d
lxiao <lxiaofeng101@gmail.com>
2020-10-31 15:36:13 +0800

    eureka 服务注册、高可用部署

--- springboot-hello/src/main/java/com/lxiaocode/hello/controller/HelloController.java
+++ springboot-hello/src/main/java/com/lxiaocode/hello/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.lxiaocode.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private DiscoveryClient client;
+
     @GetMapping("/hello")
     public String index() {
+        String services = "Services: " + client.getServices();
+        System.out.println(services);
         return "Hello World";
     }
 }
