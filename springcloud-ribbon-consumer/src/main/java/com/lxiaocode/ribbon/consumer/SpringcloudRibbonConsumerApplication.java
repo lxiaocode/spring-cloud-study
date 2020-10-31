40d5604a1783c997f902d27d666d4ae1b0dd64f4
lxiao <lxiaofeng101@gmail.com>
2020-10-31 16:04:56 +0800

    ribbon 服务发现与消费、负载均衡

---
+++ springcloud-ribbon-consumer/src/main/java/com/lxiaocode/ribbon/consumer/SpringcloudRibbonConsumerApplication.java
@@ -0,0 +1,24 @@
+package com.lxiaocode.ribbon.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudRibbonConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudRibbonConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced   // 开启负载均衡
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ springcloud-ribbon-consumer/src/test/java/com/lxiaocode/ribbon/consumer/SpringcloudRibbonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxiaocode.ribbon.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudRibbonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
