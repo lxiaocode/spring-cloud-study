c0f3b5bfb7e087901208c2c00a7590751377ad0a
lxiao <lxiaofeng101@gmail.com>
2020-11-09 22:43:19 +0800

    SpringCloud-Feign 修改 feign 继承特性的部分代码。 增加说明注释。

--- SpringCloud-Feign/Eureka-Provider/src/main/java/com/lxiaocode/eureka/controller/RefactorHelloController.java
+++ SpringCloud-Feign/Eureka-Provider/src/main/java/com/lxiaocode/eureka/controller/RefactorHelloController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lixiaofeng
  * @date 2020/11/9 上午9:44
  * @blog http://www.lxiaocode.com/
+ *
+ * 实现 Hello-Service-api 提供的接口定义。
+ * 通过继承的方式，Controller 中不再包含请求映射注解 @RequestMapping，
+ * 而参数注解定义在重写的时候会自动带过来。
  */
 @RestController
 public class RefactorHelloController implements HelloService {
--- SpringCloud-Feign/Feign-Consumer/src/main/java/com/lxiaocode/feign/service/HelloService.java
+++ SpringCloud-Feign/Feign-Consumer/src/main/java/com/lxiaocode/feign/service/HelloService.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.*;
  * @author lixiaofeng
  * @date 2020/11/9 上午12:10
  * @blog http://www.lxiaocode.com/
+ *
+ * 通过 @FeignClient 注解指定服务名来绑定服务，服务名不区分大小写。
+ * ---------------------------------------------------------------------------------------
+ * 注意：
+ *      如果我们要创建多个具有相同名字或 URL 的 Feign 客户端，使它们指向同一台服务器，
+ *      但每个客户端的配置具有不同的自定义配置，则必须使用 contextId 属性，避免 bean 冲突。
  */
-//@FeignClient(value = "hello-service", fallback = HelloServiceFallback.class)
+@FeignClient(value = "hello-service", contextId = "hello")
 public interface HelloService {
 
     @GetMapping("/hello")
     String hello();
 
+    /**
+     * 以下为参数绑定示例
+     * @param name
+     * @return
+     */
     @GetMapping("/hello1")
     String hello(@RequestParam String name);
 
